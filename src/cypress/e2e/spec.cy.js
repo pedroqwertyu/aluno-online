634eef9fa61fb5402b12327a668eaa6bc67afc1d
pedroqwertyu <pedro.h.m.f30@gmail.com>
2023-06-19 21:52:06 -0300

    contains

--- src/cypress/e2e/spec.cy.js
+++ src/cypress/e2e/spec.cy.js
@@ -1,10 +1,26 @@
-describe('Testando aluno-online2', () => {
+describe('Teste 1 aluno-online', () => {
   it('Novo Requerimento', () => {
-    cy.visit('http://localhost:3000')
-    cy.get(':nth-child(5) > .nav-link').click()
-    cy.get('.btn').click()
-    cy.get('.btn-danger').click()
-    cy.get('.btn-light').click()
+    cy.visit('http://localhost:3000/login')
+    cy.get('#inputMatricula').type('2023100')
+    cy.get('#inputSenha').type('senha123')
+    cy.get('body > form > button').click()
+    cy.get('#navbarToggler1 > ul.navbar-nav.mr-auto.mt-2.mt-lg-0 > li:nth-child(5) > a').click()
+    cy.get('body > div > div > div.card-header > a').click()
+    cy.get("#inputDisciplina").select("ADS032 - Construção de Frontend")
+    cy.get("#inputProva").select("A1")
+    cy.get('#inputArgumentacao').type('Teste Cypress')
+    cy.get('body > div > div > div.card-body > form > button').click()
+    cy.get('body > div > div > div.card-body').contains('ADS032 - Construção de Frontend')
+    cy.get('body > div > div > div.card-body').contains('Pendente')
   })
 })
+
+describe('Teste 2 aluno-online', () => {
+  it('Novo Requerimento', () => {
+    cy.visit('http://localhost:3000/requerimentos')
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get('.mr-auto > :nth-child(1) > .nav-link').click()
+  })
+})
+
 document.querySelector("#inputMatricula")
